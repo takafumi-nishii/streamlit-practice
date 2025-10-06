f8bfe16d02579705ca0fcfd74e91b84e174d0acb
takaf <takafm2438@gmail.com>
2025-10-06 11:37:49 +0900

    20251006_3

--- start_menu.py
+++ start_menu.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.title("High and Low カードゲームへようこそ！")
 st.markdown("早速、サイドバーのHigh and Low Gameをクリックしてゲームを始めよう。")
 st.markdown("詳しいルールを下記リンクから確認しておこう。")
-st.markdown("[詳しいルール](https://openai.com)")
+st.markdown("[詳しいルール](https://takafumi-nishii.github.io/streamlit-practice/)")
